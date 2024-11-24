9dfd4a33feab78499e4f6f7fb0c49ab8d20008c3
Chrimo02 <chmodel02@gmail.com>
2024-11-24 17:40:28 +0100

    Changes by has

--- src/main/java/de/thws/fiw/backendsystems/templates/jpatemplate/domain/services/RoomService.java
+++ src/main/java/de/thws/fiw/backendsystems/templates/jpatemplate/domain/services/RoomService.java
@@ -5,18 +5,66 @@ import de.thws.fiw.backendsystems.templates.jpatemplate.domain.models.Room;
 import java.time.LocalDate;
 
 public class RoomService {
-    public boolean isAvailable(Room room, LocalDate checkIn, LocalDate checkOut) {
-        for (LocalDate date = checkIn; date.isBefore(checkOut); date = date.plusDays(1)) {
-            if (!room.getAvailabilityMap().getOrDefault(date, false)) {
-                return false; // Room is not available for at least one day in the range
-            }
+    //eine Instanz von Hotelservice estellen und die getHotelById
+    private final RoomPort roomPort; //die ports mit repository ersetzen
+    private final HotelPort hotelPort;
+    public RoomService (RoomPort roomPort, HotelPort hotelPort) {
+        this.hotelPort = hotelPort;
+        this.roomPort = roomPort;
+    }
+    private Room getRoomById(long roomId) {
+
+        Room room = roomPort.findRoomById(roomId);
+        if (room == null) //wird nicht gebraucht, wenn wir Optional verwenden, oder?
+            throw new IllegalArgumentException("A room with id: " + roomId + " was not found");
+        return room;
+    }
+    private Hotel getHotelById(long hotelId) {
+        Hotel hotel = hotelPort.findHotelById(hotelId);
+        if (hotel == null) //dieselbe Frage wie oben
+            throw new IllegalArgumentException("A hotel with id:" + hotelId + "was not found");
+        return hotel;
+    }
+    public void bookRoom(long roomId, LocalDate checkIn, LocalDate checkOut) {
+        updateAvailabiltyMap(roomId, checkIn, checkOut, false);
+    }
+
+    public void cancelRoom(long roomId, LocalDate checkIn, LocalDate checkOut) {
+        updateAvailabiltyMap(roomId, checkIn, checkOut, true);
+    }
+    public void createRoom(double pricePerNight, RoomIdentifier roomIdentifier, long hotelId, Class<? extends Room> roomType) {
+        Hotel hotel = getHotelById(hotelId);
+        Room room;
+        if (roomType.equals(SingleRoom.class)) {
+            room = new SingleRoom(pricePerNight, roomIdentifier, hotel);
+        }
+        else if(roomType.equals(DoubleRoom.class)) {
+            room = new DoubleRoom(pricePerNight, roomIdentifier, hotel);
+        }
+        else throw new RuntimeException("Invalid room type!");
+        roomPort.saveRoom(room);
+    }
+    public boolean isAvailable(long roomId, LocalDate checkIn, LocalDate checkOut) {
+        Room room = getRoomById(roomId);
+        Map<LocalDate, Boolean> availabilyMap = room.getAvailabilityMap();
+        for(LocalDate i = checkIn; !i.isAfter(checkOut); i = i.plusDays(1)) {
+            if (!availabilyMap.getOrDefault(i, false))
+                return false;
         }
         return true;
     }
-
-    public void updateAvailabilityMap(Room room, LocalDate checkIn, LocalDate checkOut, boolean falseMeansBookTrueMeansCancel) {
-        for (LocalDate date = checkIn; date.isBefore(checkOut); date = date.plusDays(1)) {
-            room.getAvailabilityMap().put(date, true); // Mark dates as available again
+    public double getPriceById(long roomId) {
+        Room room = getRoomById(roomId);
+        return room.getPricePerNight();
+    }
+    public void updateAvailabiltyMap(long roomId, LocalDate checkIn, LocalDate checkOut, boolean status) {
+        Room room = getRoomById(roomId);
+        Map<LocalDate, Boolean> availabilyMap = room.getAvailabilityMap();
+        for(LocalDate i = checkIn; !i.isAfter(checkOut); i = i.plusDays(1)) {
+            availabilyMap.put(i, status);
         }
+    }
+    public boolean removeRoom(long roomId) { //boolean?
+        roomPort.removeRoom(roomId);
     }
 }
