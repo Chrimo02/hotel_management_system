b3102e1b3f33a8fd56b78e28d4429e428a6b32b2
Chrimo02 <chmodel02@gmail.com>
2024-12-19 15:41:53 +0100

    Changes by Vince

--- src/main/java/de/thws/fiw/backendsystems/templates/jpatemplate/infrastructure/persistence/dao/implementation/RoomIdentifierDAOImpl.java
+++ src/main/java/de/thws/fiw/backendsystems/templates/jpatemplate/infrastructure/persistence/dao/implementation/RoomIdentifierDAOImpl.java
@@ -1,6 +1,11 @@
 package de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.dao.implementation;
 
+import de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.dao.interfaces.RoomIdentifierDAO;
+import de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.entities.RoomIdentifierEntity;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 @ApplicationScoped
 public class RoomIdentifierDAOImpl implements RoomIdentifierDAO {
--- src/main/java/de/thws/fiw/backendsystems/templates/jpatemplate/infrastructure/persistence/repositories/adapters/RoomIdentifierDatabaseAdapter.java
+++ src/main/java/de/thws/fiw/backendsystems/templates/jpatemplate/infrastructure/persistence/repositories/adapters/RoomIdentifierDatabaseAdapter.java
@@ -1,7 +1,12 @@
 package de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.repositories.adapters;
 
+import de.thws.fiw.backendsystems.templates.jpatemplate.domain.models.RoomIdentifier;
+import de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.dao.interfaces.RoomIdentifierDAO;
+import de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.entities.RoomIdentifierEntity;
+import de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.mapper.RoomIdentifierMapper;
 import de.thws.fiw.backendsystems.templates.jpatemplate.infrastructure.persistence.repositories.interfaces.RoomIdentifierRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class RoomIdentifierDatabaseAdapter implements RoomIdentifierRepository {
